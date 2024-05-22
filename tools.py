a7be909b99482479ce3a760d44363f5eea752ead
YiQiuZHAO <52433327+YiQiuZHAO@users.noreply.github.com>
2024-05-22 14:45:52 +0800

    Update tools.py

--- tools.py
+++ tools.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 
 
-def img_show(imgs):
+def img_show(imgs, key=0, des=True):
     if type(imgs) == list:
         for i, img in enumerate(imgs):
             cv2.namedWindow(str(i), cv2.WINDOW_NORMAL)
@@ -12,5 +12,9 @@ def img_show(imgs):
         cv2.namedWindow('frame', cv2.WINDOW_NORMAL)
         img = (imgs * (255 / imgs.max())).astype('uint8')
         cv2.imshow('frame', img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    k = cv2.waitKey(key)
+    else:
+        return
+    if des:
+        cv2.destroyAllWindows()
+    return k
